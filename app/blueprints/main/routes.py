966adcf07938f63ac24735aeca813d3387c7f6bd
KhoaThaiisme <khoathai@khoas-MacBook-Pro.local>
2023-05-09 00:14:27 -0500

    first commit

---
+++ app/blueprints/main/routes.py
@@ -0,0 +1,11 @@
+from flask import render_template
+
+from . import bp
+
+@bp.route('/')
+def home():
+    return render_template('index.j2', title='Home')
+
+@bp.route('/inventory')
+def about():
+    return render_template('about.j2')
